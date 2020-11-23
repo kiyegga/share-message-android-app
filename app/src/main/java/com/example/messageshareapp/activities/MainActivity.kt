068f5d8c4cb91f5f8ad087a8a6054390917ab290
kiyegga francis <kiyeggafrancis1@gmail.com>
2020-11-23 21:42:18 +0300

    first commit for all project

--- app/src/main/java/com/example/messageshareapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/messageshareapp/activities/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.messageshareapp.R
 import com.example.messageshareapp.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
